f06f1191a4e12e609a0ce9dd503eb4befcd5e08e
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2026-05-12 17:52:00 +0100

    Move World endpoints to WorldEndpoints.cs

---
+++ Endpoints/WorldEndpoints.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TantareApi.Entities;
+using TantareApi.Infrastructure;
+
+namespace TantareApi.Endpoints
+{
+    public static class WorldEndpoints
+    {
+        public static void MapWorldEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/worlds");
+
+            group.MapPost("/", async (World world, TantareDb db) =>
+            {
+                db.Worlds.Add(world);
+                await db.SaveChangesAsync();
+                return TypedResults.Created();
+            });
+
+            group.MapGet("/", async (TantareDb db) =>
+            {
+                return await db.Worlds.ToListAsync();
+            });
+
+            group.MapGet("/{id}", async Task<Results<Ok<World>, NotFound>> (int id, TantareDb db) =>
+            {
+                return await db.Worlds.FindAsync(id)
+                    is World world
+                        ? TypedResults.Ok(world)
+                        : TypedResults.NotFound();
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using TantareApi.Entities;
 using TantareApi.Infrastructure;
-using Microsoft.AspNetCore.Http.HttpResults;
+using TantareApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TantareDb>(opt => opt.UseInMemoryDatabase("Tantare"));
 
 var app = builder.Build();
 
-app.MapPost("/worlds", async (TantareDb db, World world) =>
-{
-    db.Worlds.Add(world);
-    await db.SaveChangesAsync();
-    return TypedResults.Created();
-});
-
-app.MapGet("/worlds", async (TantareDb db) =>
-{
-    return await db.Worlds.ToListAsync();
-});
-
-app.MapGet("/worlds/{id}", async Task<Results<Ok<World>, NotFound>> (int id, TantareDb db) =>
-{
-    return await db.Worlds.FindAsync(id)
-        is World world
-            ? TypedResults.Ok(world)
-            : TypedResults.NotFound();
-});
+app.MapWorldEndpoints();
 
 app.Run();
