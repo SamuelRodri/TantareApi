0d486bbc4188cd3a776ad84a6de5f59b178808e2
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2026-05-12 16:50:00 +0100

    Installed and configured EF Core for in memory DB

---
+++ Infrastructure/TantareDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TantareApi.Entities;
+
+namespace TantareApi.Infrastructure
+{
+    public class TantareDb : DbContext
+    {
+        public TantareDb(DbContextOptions<TantareDb> options) : base(options) { }
+
+        public DbSet<World> Worlds => Set<World>();
+        public DbSet<Location> Locations => Set<Location>();
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using TantareApi.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TantareDb>(opt => opt.UseInMemoryDatabase("Tantare"));
 
 var app = builder.Build();
 
