9437b9d4fda9a2849163b71395f0b2b208e0bc79
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2026-05-12 17:16:00 +0100

    Basic World entity endpoints in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using TantareApi.Entities;
 using TantareApi.Infrastructure;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TantareDb>(opt => opt.UseInMemoryDatabase("Tantare"));
 
 var app = builder.Build();
+
+app.MapPost("/worlds", async (TantareDb db, World world) =>
+{
+    db.Worlds.Add(world);
+    await db.SaveChangesAsync();
+    return TypedResults.Created();
+});
+
+app.MapGet("/worlds", async (TantareDb db) =>
+{
+    return await db.Worlds.ToListAsync();
+});
+
+app.MapGet("/worlds/{id}", async Task<Results<Ok<World>, NotFound>> (int id, TantareDb db) =>
+{
+    return await db.Worlds.FindAsync(id)
+        is World world
+            ? TypedResults.Ok(world)
+            : TypedResults.NotFound();
+});
 
 app.Run();
