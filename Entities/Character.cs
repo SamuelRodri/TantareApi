c7ff63cb3aebd9921a6939d316da06ad07ed8a29
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2026-05-12 16:33:47 +0100

    Initial entities created

---
+++ Entities/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace TantareApi.Entities
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public DateTime? birthDate { get; set; }
+        public DateTime? deathDate { get; set;  }
+    }
+}
---
+++ Entities/World.cs
@@ -0,0 +1,8 @@
+﻿namespace TantareApi.Entities
+{
+    public class World
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
