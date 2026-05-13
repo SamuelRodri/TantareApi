45e2df09e02b9a6a9828b01dd8922e6c02a7f792
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2026-05-13 20:49:01 +0100

    Added Swagger

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@ using TantareApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TantareDb>(opt => opt.UseInMemoryDatabase("Tantare"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapWorldEndpoints();
 app.MapLocationEndpoints();
